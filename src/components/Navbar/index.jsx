77e3a3ed9d544d241e263fbe688733bed347942b
likuor <likuor0528@gmail.com>
2023-11-23 16:34:05 -0800

    add: react router

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,12 +3,15 @@ import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import RestoreIcon from '@mui/icons-material/Restore';
 import ArchiveIcon from '@mui/icons-material/Archive';
+import { useNavigate } from 'react-router-dom';
 
 export default function Navbar() {
-  const [value, setValue] = React.useState('feed');
+  const [value, setValue] = React.useState('');
+  const navigate = useNavigate();
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
+    navigate('/' + newValue);
   };
 
   return (
@@ -17,11 +20,8 @@ export default function Navbar() {
       value={value}
       onChange={handleChange}
     >
-      <BottomNavigationAction
-        label='Feed'
-        value='feed'
-        icon={<RestoreIcon />}
-      />
+      {/* value is empty because the default path */}
+      <BottomNavigationAction label='Feed' value='' icon={<RestoreIcon />} />
       <BottomNavigationAction
         label='Archived'
         value='archived'
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
 
 import './css/body.css';
 import './css/app.css';
 import './css/header.css';
 
 const container = document.getElementById('app');
-const root = createRoot(container); // createRootを直接呼び出す
+const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
